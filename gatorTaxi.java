32e6187b69082ff6087d0ff6f610bfddfe837eb3
vedantspatil <37808420+vedantspatil@users.noreply.github.com>
2023-11-20 11:06:00 -0500

    Update gatorTaxi.java

--- gatorTaxi.java
+++ gatorTaxi.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 //Main class to run the program
 public class gatorTaxi {
@@ -17,6 +14,7 @@ public class gatorTaxi {
 
 		// setting a flag 'terminate' to terminate code when duplicate ride is found
 		boolean terminate = false;
+		int ColorFlipCount = 0;
 		List<String> wholeInput = new ArrayList<>();
 		String input;
 		RedBlackTree rbtObj = new RedBlackTree();
@@ -32,20 +30,19 @@ public class gatorTaxi {
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
 		}
-		
-		// processing the input
-		for (String s : wholeInput) {
+		try {
+			for (String s : wholeInput) {
 			input = s;
 			String[] arr = input.split("\\(");
-			input = arr[0];
+			input = arr[0].trim();
 			switch (input) {
 				case "PrintBook":
 					String[] bookIds = arr[1].split("\\)");
 					// searching the tree for the rideNumber in the input
-					TreeNode book = rbtObj.search(Integer.parseInt(bookIds[0]));
+					TreeNode book = rbtObj.search(Integer.parseInt(bookIds[0].trim()));
 					// if no such ride is found, print(0,0,0)
 					if (book == null) {
-						System.out.println("Book "+ bookIds[0] + " not found in the Library\n");
+						System.out.println("Book "+ bookIds[0].trim() + " not found in the Library\n");
 					} else {
 						// printing the ride details of the found ride
 						rbtObj.printNode(book);
@@ -55,11 +52,11 @@ public class gatorTaxi {
 					bookIds = arr[1].split("\\)")[0].split(",");
 					// printing the ride details of the rides found in the given range
 					ArrayList<TreeNode> nodesInRange = rbtObj.findNodesInRange(rbtObj.getRoot(), Integer.parseInt(bookIds[0]),
-							Integer.parseInt(bookIds[1]));
+							Integer.parseInt(bookIds[1].trim()));
 					// if no rides are found in the given range
 					// Change
 					if (nodesInRange.size() == 0) {
-						System.out.println("No books are found in the given range");
+						System.out.println("No books are found in the given range\n");
 					}
 					else{
 						for(TreeNode node: nodesInRange){
@@ -69,21 +66,34 @@ public class gatorTaxi {
 					break;
 
 				case "InsertBook":
+					HashMap<Integer, Integer> oldTree = new HashMap<>();
+					rbtObj.inOrderTraversalofTree(rbtObj.getRoot(), oldTree);
+
 					String[] bookDetails = arr[1].split("\\)")[0].split(",");
-					book = rbtObj.search(Integer.parseInt(bookDetails[0]));
+					book = rbtObj.search(Integer.parseInt(bookDetails[0].trim()));
 					
 					// if ride already exists for the given ride number
 					if (book != null) {
-						System.out.println("Duplicate Book");
+						System.out.println("Duplicate Book\n");
 						break;
 					} else {
 						// inserting ride into Red Black Tree
-						rbtObj.insert(Integer.parseInt(bookDetails[0]),
+						rbtObj.insert(Integer.parseInt(bookDetails[0].trim()),
 									 bookDetails[1],
 									 bookDetails[2],
 									 bookDetails[3]
 									 );
 
+					}
+					HashMap<Integer, Integer> newTree = new HashMap<>();
+					rbtObj.inOrderTraversalofTree(rbtObj.getRoot(), newTree);
+
+					for (Map.Entry<Integer,Integer> entry : newTree.entrySet()){
+						if(oldTree.containsKey(entry.getKey())){
+							if(oldTree.get(entry.getKey()) != entry.getValue()){
+								ColorFlipCount++;
+							}
+						}
 					}
 					break;
 
@@ -92,21 +102,20 @@ public class gatorTaxi {
 					book = rbtObj.search(Integer.parseInt(bookDetails[1].trim()));
 					//book doesnt exist
 					if (book == null) {
-						System.out.println("Book does not exist");
+						System.out.println("Book does not exist\n");
 						break;
 					}
 					//if book availabile
-
-					if(book.availability.toString().trim().equals("Yes")){
+					if(book.availability.toString().trim().equals(new String("\"Yes\""))){
 						book.availability = "No";
 						book.borrowedBy = bookDetails[0];
-						System.out.println("Book borrowed by Patron "+ bookDetails[0]);
+						System.out.println("Book "+ bookDetails[1].trim()+ " Borrowed by Patron "+ bookDetails[0].trim()+"\n");
 					}else{
-						UserNode newPatron = new UserNode(bookDetails[0],
-											 Integer.parseInt(bookDetails[2]), 
+						UserNode newPatron = new UserNode(bookDetails[0].trim(),
+											 Integer.parseInt(bookDetails[2].trim()), 
 											new Timestamp(System.currentTimeMillis()));
 						book.reservationHeap.add(newPatron);
-						System.out.println("Book "+ book.bookId +" Reserved by Patron " + bookDetails[0]);
+						System.out.println("Book "+ book.bookId +" Reserved by Patron " + bookDetails[0]+"\n");
 					}
 
 					break;
@@ -114,51 +123,67 @@ public class gatorTaxi {
 				case "ReturnBook":
 					bookDetails = arr[1].split("\\)")[0].split(",");
 					book = rbtObj.search(Integer.parseInt(bookDetails[1].trim()));
+
+					
 					if (book == null) {
-						System.out.println("Book does not exist");
+						System.out.println("Book does not exist\n");
 						break;
 					}
-					System.out.println("Book " + bookDetails[1] + " returned by patron "+ bookDetails[0]);
+					System.out.println("Book " + bookDetails[1].trim() + " Returned by patron "+ bookDetails[0]+ "\n");
 					//if book availabile
-			
+
 					if(book.reservationHeap.isEmpty()){
-						book.availability = "Yes";
+						book.availability = "\"Yes\"";
 						book.borrowedBy = "None";
 					}else{
 						UserNode newPatron = book.reservationHeap.poll();
 						book.borrowedBy = newPatron.userId;
-						System.out.println("Book "+ book.bookId +" allocated to Patron " + newPatron.userId);
+						System.out.println("Book "+ book.bookId +" Allocated to Patron " + newPatron.userId +"\n");
 					}
 
 					break;
 
 				case "DeleteBook":
+					oldTree = new HashMap<>();
+					rbtObj.inOrderTraversalofTree(rbtObj.getRoot(), oldTree);
+
 					String bookId = arr[1].split("\\)")[0];
 					book = rbtObj.search(Integer.parseInt(bookId.trim()));
 					if (book == null) {
-						System.out.println("Book does not exist");
+						System.out.println("Book does not exist\n");
 						break;
 					}
 
-					String displayString = "Book "+bookId+" is no longer available. ";
+					String displayString = "Book "+bookId+" is no longer available.";
 					if (book.reservationHeap.size()>=1){
 						if(book.reservationHeap.size()>1){
 							displayString+="Reservations made by Patrons ";
 							for(UserNode node: book.reservationHeap){
 								displayString+=node.userId+" ";
 							}
-							displayString+="have been cancelled!";
+							displayString+="have been cancelled!.";
 						}else{
 							displayString+="Reservation made by Patron "+ book.reservationHeap.poll().userId + " have been cancelled!";
 						}
 					}
-					rbtObj.deleteNode(Integer.parseInt(bookId));
+					displayString+="\n";
 					System.out.println(displayString);
+					rbtObj.deleteNode(Integer.parseInt(bookId.trim()));
+
+					newTree = new HashMap<>();
+					rbtObj.inOrderTraversalofTree(rbtObj.getRoot(), newTree);
+					for (Map.Entry<Integer,Integer> entry : newTree.entrySet()){
+						if(oldTree.containsKey(entry.getKey())){
+							if(oldTree.get(entry.getKey()) != entry.getValue()){
+								ColorFlipCount++;
+							}
+						}
+					}
 					break;
-				
+
 				case "FindClosestBook":
 					bookId = arr[1].split("\\)")[0];
-					ArrayList<Integer> arrayList = rbtObj.printClosest(Integer.parseInt(bookId));
+					ArrayList<Integer> arrayList = rbtObj.printClosest(Integer.parseInt(bookId.trim()));
 					for(int book_id: arrayList){
 						TreeNode fNode = rbtObj.search(book_id);
 						rbtObj.printNode(fNode);
@@ -166,46 +191,13 @@ public class gatorTaxi {
 					break;
 
 				case "ColorFlipCount":
-					System.out.println(rbtObj.getRoot().bookId);
+					System.out.println("Color Flip Count: "+ ColorFlipCount+"\n");;
 					break;
 
-				// 	// searching the red black tree using ride number
-				// 	ride = rbtObj.search(rideNo);
-
-				// 	// if no such ride found, move to next input
-				// 	if (ride == null)
-				// 		break;
-				// 	// case 1 of update trip scenario
-				// 	if (new_duration <= ride.tripDuration) {
-				// 		// delete the existing ride
-				// 		HeapNode delNode4 = minHeap.deleteKey(ride.node.idx);
-				// 		rbtObj.deleteNode(delNode4.rbt.rideNumber);
-
-				// 		// insert new ride with updated ride cost
-				// 		HeapNode insertedNode = minHeap.insert(new HeapNode(rideNo, delNode4.rideCost, new_duration));
-				// 		insertedNode.rbt = rbtObj.insert(insertedNode.rideNumber, insertedNode.rideCost,
-				// 				insertedNode.tripDuration, insertedNode);
-				// 		// case 2 of update trip scenario
-				// 	} else if ((ride.tripDuration < new_duration) && (new_duration <= (2 * ride.tripDuration))) {
-
-				// 		int newCost = ride.rideCost + 10;
-
-				// 		// delete the existing ride
-				// 		HeapNode delNode2 = minHeap.deleteKey(ride.node.idx);
-				// 		rbtObj.deleteNode(delNode2.rbt.rideNumber);
-
-				// 		// insert new ride with updated ride cost
-				// 		HeapNode insertedNode = minHeap.insert(new HeapNode(rideNo, newCost, new_duration));
-				// 		insertedNode.rbt = rbtObj.insert(insertedNode.rideNumber, insertedNode.rideCost,
-				// 				insertedNode.tripDuration, insertedNode);
-				// 		// case 3 of update trip scenario
-				// 	} else if (new_duration > (2 * ride.tripDuration)) {
-
-				// 		// delete the existing ride from min heap and red black tree
-				// 		HeapNode delNode3 = minHeap.deleteKey(ride.node.idx);
-				// 		rbtObj.deleteNode(delNode3.rbt.rideNumber);
-				// 	}
-				// 	break;
+				case "Quit":
+					System.out.println("Program Terminated!!\n");
+					System.exit(0);
+					break;
 
 			}
 			// terminating the program in case of inserting a duplicate ride
@@ -213,5 +205,11 @@ public class gatorTaxi {
 				break;
 		}
 
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid Input\n");
+			System.exit(0);
+		}
+		// processing the input
+		
 	}
 }
